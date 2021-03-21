8f8a40827d47418d07662eb45ed682d64448f45d
Ubuntu <ubuntu@ip-172-31-23-91.ap-northeast-1.compute.internal>
2021-03-21 01:45:22 +0000

    fix mcv

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,9 +1,30 @@
 class MemosController < ApplicationController
   def new
+  end  
+  
+  def edit
+    @memo = Memo.find_by(id: params[:id])
   end
+   
+  def update
+    @memo = Memo.find_by(id: params[:id])
+    @memo.content = params[:content]
+    @memo.save
+    
+    redirect_to("/")
+  end  
+  
   def create
+  
     @memo = Memo.new(content: params[:content])
     @memo.save
+    redirect_to("/")
+  end  
+  
+  def destroy
+    @memo = Memo.find_by(id: params[:id])
+    @memo.destroy
+    
     redirect_to("/")
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get "/" => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/" => "home#top"
   get "/new" => "memos#new"
-  
   post "/create" =>  "memos#create"
+  get "/:id/edit" => "memos#edit"
+  post "/:id/update" => "memos#update"
+  post "/:id/destroy" => "memos#destroy"
+  
 end
